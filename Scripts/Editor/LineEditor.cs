e9e0048622f7984202a434d1246b0971795c90e8
Porrith Suong <psuong95@gmail.com>
2018-05-20 18:15:21 -0400

    Adds a line.

---
+++ Scripts/Editor/LineEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Curves.EditorTools {
+    
+    [CustomEditor(typeof(Line))]
+    public class LineEditor : Editor {
+
+        private SerializedProperty p0;
+        private SerializedProperty p1;
+        
+        private void OnEnable() {
+            p0 = serializedObject.FindProperty("p0");
+            p1 = serializedObject.FindProperty("p1");
+        }
+
+        private void OnSceneGUI() {
+            using (var changeCheck = new EditorGUI.ChangeCheckScope()) {
+                serializedObject.Update();
+                // Draw the line
+                DrawLine();
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        private void DrawLine() {
+            Handles.color = Color.green;
+            Handles.DrawLine(p0.vector3Value, p1.vector3Value);
+        }
+    }
+}
