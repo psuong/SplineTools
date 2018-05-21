b1a672d90afb715abae4dbf6ea9e468c6d27e438
Porrith Suong <psuong95@gmail.com>
2018-05-20 22:45:01 -0400

    Adds functionality to ensure the number of control points equal to the number of points.

--- Scripts/Editor/BezierEditor.cs
+++ Scripts/Editor/BezierEditor.cs
@@ -1,18 +1,52 @@
 ﻿using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Curves.EditorTools {
     [CustomEditor(typeof(Bezier))]
     public class BezierEditor : Editor {
 
-        private const float HandleSize = 0.075f;
+        private const float HandleSize = 0.15f;
 
         private SerializedProperty points;
         private SerializedProperty controlPoints;
+        private ReorderableList pointsList;
+        private ReorderableList ctrlPtsList;
         
         private void OnEnable() {
             points = serializedObject.FindProperty("points");
             controlPoints = serializedObject.FindProperty("controlPoints");
+
+            pointsList = new ReorderableList(serializedObject, points);
+            ctrlPtsList = new ReorderableList(serializedObject, controlPoints, true, true, false, false);
+
+            pointsList.drawHeaderCallback = DrawPointHeader;
+            pointsList.drawElementCallback = DrawPointElem;
+            pointsList.onAddCallback = (ReorderableList list) => {
+                controlPoints.arraySize += 2;
+                points.arraySize += 1;
+            };
+
+            ctrlPtsList.drawHeaderCallback = DrawControlPointHeader;
+            ctrlPtsList.drawElementCallback = DrawControlPointElem;
+        }
+
+        private void OnDisable() {
+            pointsList.drawHeaderCallback -= DrawPointHeader;
+            pointsList.drawElementCallback -= DrawPointElem;
+
+            ctrlPtsList.drawHeaderCallback -= DrawControlPointHeader;
+            ctrlPtsList.drawElementCallback -= DrawControlPointElem;
+        }
+
+        public override void OnInspectorGUI() {
+            using (var changeCheck = new EditorGUI.ChangeCheckScope()) {
+                serializedObject.Update();
+                DrawDefaultInspector();
+                pointsList.DoLayoutList();
+                ctrlPtsList.DoLayoutList();
+                serializedObject.ApplyModifiedProperties();
+            }
         }
 
         private void OnSceneGUI() {
@@ -26,7 +60,6 @@ namespace Curves.EditorTools {
         }
 
         private void DrawBezierCurve() {
-            Handles.color = Color.green;
             var size = points.arraySize;
             for (int i = 1; i < size; i++) {
                 var start = points.GetArrayElementAtIndex(i - 1);
@@ -40,6 +73,7 @@ namespace Curves.EditorTools {
         }
 
         private void DrawPoints() {
+            Handles.color = Color.cyan;
             var size = points.arraySize;
             for (int i = 0; i < size; i++) {
                 var element = points.GetArrayElementAtIndex(i);
@@ -49,6 +83,7 @@ namespace Curves.EditorTools {
         }
 
         private void DrawControlPoints() {
+            Handles.color = Color.green;
             var size = controlPoints.arraySize;
             for (int i = 0; i < size; i++) {
                 var element = controlPoints.GetArrayElementAtIndex(i);
@@ -56,5 +91,28 @@ namespace Curves.EditorTools {
                 element.vector3Value = Handles.FreeMoveHandle(element.vector3Value, Quaternion.identity, HandleSize, snapSize, Handles.DotHandleCap);
             }
         }
+
+#region ReorderableList
+        private void DrawPointHeader(Rect r) {
+            EditorGUI.LabelField(r, "Points", EditorStyles.boldLabel);
+        }
+
+        private void DrawControlPointHeader(Rect r) {
+            EditorGUI.LabelField(r, "Control Points", EditorStyles.boldLabel);
+        }
+
+        private void DrawControlPointElem(Rect r, int i, bool isActive, bool isFocused) {
+            DrawVectorElement(controlPoints, r, i, isActive, isFocused);
+        }
+
+        private void DrawPointElem(Rect r, int i, bool isActive, bool isFocused) {
+            DrawVectorElement(controlPoints, r, i, isActive, isFocused);
+        }
+
+        private void DrawVectorElement(SerializedProperty prop, Rect r, int i, bool isActive, bool isFocused) {
+            var elem = prop.GetArrayElementAtIndex(i);
+            EditorGUI.PropertyField(r, elem, GUIContent.none);
+        }
+#endregion
     }
 }
