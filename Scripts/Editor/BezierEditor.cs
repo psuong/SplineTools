4caf33117d9f2635de80dc36239b1edbb8f4312e
Porrith Suong <psuong95@gmail.com>
2018-05-25 19:18:41 -0400

    Adds base class for SceneView Editors.

--- Scripts/Editor/BezierEditor.cs
+++ Scripts/Editor/BezierEditor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Curves.EditorTools {
 
     [CustomEditor(typeof(Bezier))] 
-    public class BezierEditor : Editor { 
+    public class BezierEditor : SceneViewEditor { 
         private const float HandleSize = 0.07f;
 
         private SerializedProperty points;
@@ -13,13 +13,9 @@ namespace Curves.EditorTools {
 
         private ReorderableList pointsList;
         private ReorderableList controlPointsList;
-
-        private TransformData transformData;
-        private string jsonDirectory;
-        private string jsonPath;
     
-        private void OnEnable() {
-            jsonDirectory = System.IO.Path.Combine(Application.dataPath, "Scripts", "Editor", "Configurations");
+        protected override void OnEnable() {
+            base.OnEnable();
             jsonPath = System.IO.Path.Combine(jsonDirectory, string.Format("{0}.json", (target as Bezier).name));
 
             points = serializedObject.FindProperty("points");
@@ -27,9 +23,6 @@ namespace Curves.EditorTools {
 
             pointsList = new ReorderableList(serializedObject, points);
             controlPointsList = new ReorderableList(serializedObject, controlPoints, true, true, false, false);
-
-            // Register the sceneview
-            SceneView.onSceneGUIDelegate += OnSceneGUI;
 
             pointsList.drawHeaderCallback = DrawPointHeader;
             pointsList.drawElementCallback = DrawPointElement;
@@ -41,11 +34,9 @@ namespace Curves.EditorTools {
             controlPointsList.drawHeaderCallback = DrawControlPointHeader;
             controlPointsList.drawElementCallback = DrawControlPointElement;
             controlPointsList.elementHeightCallback = ElementHeight;
-        }
-
-        private void OnDisable() {
-            // Remove the sceneview 
-            SceneView.onSceneGUIDelegate -= OnSceneGUI;
+        } 
+        protected override void OnDisable() {
+            base.OnDisable();
 
             pointsList.drawHeaderCallback -= DrawPointHeader;
             pointsList.drawElementCallback -= DrawPointElement;
@@ -147,41 +138,7 @@ namespace Curves.EditorTools {
         }
 #endregion
 
-#region Transform
-        private void DrawTransformField() {
-            EditorGUILayout.LabelField("Transform", EditorStyles.boldLabel);
-            transformData.position = EditorGUILayout.Vector3Field(new GUIContent("Position"), transformData.position);
-            transformData.rotation = EditorGUILayout.Vector3Field(new GUIContent("Rotation"), transformData.rotation);
-            transformData.scale = EditorGUILayout.Vector3Field(new GUIContent("Scale"), transformData.scale);
-            transformData.showTransformData = EditorGUILayout.Toggle("Show Transform", transformData.showTransformData);
-        }
-
-        private void DrawTransformHandle() {
-            if (transformData.showTransformData) {
-                transformData.position = Handles.PositionHandle(transformData.position, Quaternion.Euler(transformData.rotation));
-                transformData.rotation = Handles.RotationHandle(Quaternion.Euler(transformData.rotation), transformData.position).eulerAngles;
-                transformData.scale = Handles.ScaleHandle(transformData.scale, transformData.position, Quaternion.Euler(transformData.rotation), transformData.scale.magnitude);
-            }
-        }
-
-        private void LoadTransformData() {
-            if (System.IO.File.Exists(jsonPath)) {
-                var jsonRep = System.IO.File.ReadAllText(jsonPath);
-                transformData = JsonUtility.FromJson<TransformData>(jsonRep);
-            } else {
-                transformData = TransformData.CreateTransformData();
-                System.IO.Directory.CreateDirectory(jsonDirectory);
-                System.IO.File.WriteAllText(jsonPath, JsonUtility.ToJson(transformData));
-            }
-        }
-
-        private void SaveTransformData() {
-            var json = JsonUtility.ToJson(transformData);
-            System.IO.File.WriteAllText(jsonPath, json);
-        }
-#endregion
-
-        private void OnSceneGUI(SceneView sceneView) {
+        protected override void OnSceneGUI(SceneView sceneView) {
             using (var changeCheck = new EditorGUI.ChangeCheckScope()) {
                 LoadTransformData();
                 serializedObject.Update();
