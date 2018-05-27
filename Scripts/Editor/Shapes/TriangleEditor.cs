ff9ed160956c13a2dacffed48fee5095c6afc5cd
Porrith Suong <psuong95@gmail.com>
2018-05-27 15:03:34 -0400

    Adds a mesh generator base class.

---
+++ Scripts/Editor/Shapes/TriangleEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Curves.EditorTools {
+
+    [CustomEditor(typeof(Triangle))]
+    public class TriangleEditor : Editor {
+        
+        public override void OnInspectorGUI() {
+            using (var changeCheck = new EditorGUI.ChangeCheckScope()) {
+                DrawDefaultInspector();
+                DrawMeshGeneratorButton();
+            }
+        }
+
+        private void DrawMeshGeneratorButton() {
+            if (GUILayout.Button(new GUIContent("Generate Mesh"))) {
+                Triangle instance = target as Triangle;
+                instance.GenerateMesh();
+            }
+        }
+    }
+}
---
+++ Scripts/Samples/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Curves {
+
+    /**
+     * This will just create a single triangle.
+     * Normal: Vector3.up
+     * UVs: Depends on the coordinates of a unit square.
+     * Points: Any value
+     */
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class Triangle : MonoBehaviour {
+
+        public Vector3[] points;
+        public Vector3[] normals;
+        public Vector2[] uvs;
+        public int[] triangleIndices;
+
+        private MeshFilter meshFilter;
+        private MeshGenerator meshGenerator;
+
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.red;
+            var radius = 0.2f;
+            try {
+                foreach (var point in points) {
+                    Gizmos.DrawWireSphere(point, radius);
+                }
+            } catch(System.Exception) {}
+        }
+
+        public void GenerateMesh() {
+            meshFilter = GetComponent<MeshFilter>();
+            meshGenerator = new MeshGenerator();
+
+            meshGenerator.AddVertices(points);
+            meshGenerator.AddNormals(normals);
+            meshGenerator.AddUVs(uvs);
+            meshGenerator.AddTriangle(triangleIndices);
+
+            meshFilter.sharedMesh = meshGenerator.CreateMesh();
+        }
+    }
+}
