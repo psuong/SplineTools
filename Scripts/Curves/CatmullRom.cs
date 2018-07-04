65f12dd3b5f1df5f9806acf1c950babef51bda18
Porrith Suong <psuong95@gmail.com>
2018-07-04 13:18:53 -0400

    Fixes the tangent calculation of the Catmull Rom.

--- Scripts/Curves/CatmullRom.cs
+++ Scripts/Curves/CatmullRom.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using CommonStructures;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Curves {
@@ -37,6 +38,34 @@ namespace Curves {
             }
 
             return pts.ToArray();
+        }
+
+        /// <summary>
+        /// Returns an array of normalised tangents for each parametric value t within the Catmull Rom Spline.
+        /// </summary>
+        /// <param name="segments">How many line segments are there within each spline.</param>
+        /// <returns>An array of tuples defining the direction relative to the original point.</returns>
+        public Tuple<Vector3, Vector3>[] SampleTangents(int segments) {
+            var directions = new List<Tuple<Vector3, Vector3>>();
+
+            for (int i = 0; i < points.Length; i++) {
+                if ((i == 0 || i == points.Length - 1 || i == points.Length - 2) && !isLooping) {
+                    continue;
+                }
+
+                var p0 = points[ClampIndex(i - 1)];
+                var p1 = points[ClampIndex(i)];
+                var p2 = points[ClampIndex(i + 1)];
+                var p3 = points[ClampIndex(i + 2)];
+
+                for (int t = 0; t <= segments; t++) {
+                    var progress = (float) t / segments;
+                    var pt = CatmullRom.GetCatmullRomSplinePoint(p0, p1, p2, p3, progress);
+                    var tangent = CatmullRom.GetTangent(p0, p1, p2, p3, progress);
+                    directions.Add(Tuple<Vector3, Vector3>.CreateTuple(pt, tangent));
+                }
+            }
+            return directions.ToArray();
         }
 
         private int ClampIndex(int i) {
@@ -81,9 +110,9 @@ namespace Curves {
         /// <param name="p2">The end point of the actual spline.</param>
         /// <param name="p3">The second control point.</param>
         /// <param name="t">Parametric value, t, which defines the position of the point on the curve.</param>
-        /// <returns>The first derivative along the point of the curve.</returns>
+        /// <returns>The first derivative along the point of the curve normalised.</returns>
         public static Vector3 GetTangent(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
-            var a = t * p2 + t * p0;
+            var a = p2 - p0;
             var b = (2 * t) * (2 * p0 - 5f * p1 + 4f * p2 - p3);
             var c = (3 * t * t) * (-p0 + 3f * p1 - 3f * p2 + p3);
             
